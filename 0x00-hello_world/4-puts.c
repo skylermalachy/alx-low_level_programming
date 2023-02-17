ee269b0a96a29a05eb7dc09f9b5fb64b563c2531
skylermalachy <skylermalachy@gmail.com>
2023-02-17 13:37:02 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  *   * main-Entry point
  *     * Return: 0 if successful
- *       */
+ */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
