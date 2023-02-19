75f7b9c8497b1e0b919bb909c14066305c2ffd11
skylermalachy <skylermalachy@gmail.com>
2023-02-19 04:39:54 -0800

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
-/*
- *  * main - print lower and upper using putcha
- *   * Return: Always 0 (Success)
+
+/**
+ *  * main - prints the alphabet in lowercase, and then in uppercase,
+ *   * followed by a new line
+ *    * Return: Always 0 (Success)
  */
 int main(void)
 {
-char lower = 'a';
-char upper = 'A';
-while (lower <= 'z')
-{
-putchar(lower);
-lower++;
-}
-while (upper <= 'Z')							
-{
-putchar(upper);
-upper++;
-}
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+for (ch = 'A'; ch <= 'Z'; ch++)
+putchar(ch);
 putchar('\n');
 return (0);
 }
